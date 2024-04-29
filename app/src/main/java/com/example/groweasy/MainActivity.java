d018561f08aec7c116b4dfc7f60dde4b2111c02c
Bianca <bianca.munozjaramillo@gmail.com>
2024-04-29 15:06:24 -0400

    FIRST COMMIT

---
+++ app/src/main/java/com/example/groweasy/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.groweasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnIngresar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this,Dashboard_1.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
