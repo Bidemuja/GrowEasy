27e530c6f86b2958eb8f0103183843580e68053b
Bianca <bianca.munozjaramillo@gmail.com>
2024-05-01 18:14:49 -0400

    Se agrega dashboard.xml y main_menu.xml, para realizar pruebas.

--- app/src/main/java/com/example/groweasy/MenuTrabajador.java
+++ app/src/main/java/com/example/groweasy/MenuTrabajador.java
@@ -11,17 +11,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuTrabajador extends AppCompatActivity {
 
-    Button btnImgMenu;
+    Button btnImgMenu, btnHumedad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_1);
         btnImgMenu = (Button) findViewById(R.id.imgLogoMenu);
+        btnHumedad = (Button) findViewById(R.id.btnHumedad);
         btnImgMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent;
                 intent = new Intent(MenuTrabajador.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHumedad.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MenuTrabajador.this, Dashboard.class);
                 startActivity(intent);
             }
         });
