7bec809c36dd38b574d085c936818ab1ef436a58
gabriel <gabriel2000@outlook.cl>
2024-05-17 13:45:04 -0400

    implementacion completa de lectura en firebase

--- app/src/main/java/com/example/groweasy/MenuTrabajador.java
+++ app/src/main/java/com/example/groweasy/MenuTrabajador.java
@@ -1,12 +1,9 @@
 package com.example.groweasy;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,10 +27,10 @@ public class MenuTrabajador extends AppCompatActivity {
         btnImgMenu = (Button) findViewById(R.id.imgLogoMenu);
         btnHumedad = (Button) findViewById(R.id.btnHumedad);
 
-        txthumedad= (TextView) findViewById(R.id.textView3);
-        txtluz= (TextView) findViewById(R.id.textView3);
-        txtambiente= (TextView) findViewById(R.id.textView3);
-        txtsuelo= (TextView) findViewById(R.id.textView3);
+        txthumedad= (TextView) findViewById(R.id.txthumedad);
+        txtluz= (TextView) findViewById(R.id.txtluz);
+        txtambiente= (TextView) findViewById(R.id.txtambiente);
+        txtsuelo= (TextView) findViewById(R.id.txtsuelo);
         groweasydb= FirebaseDatabase.getInstance().getReference();
 
         groweasydb.child("Humedad").addValueEventListener(new ValueEventListener() {
@@ -42,6 +39,51 @@ public class MenuTrabajador extends AppCompatActivity {
                 if (dataSnapshot.exists()) {
                     String humedad = dataSnapshot.child("humedad").getValue().toString();
                     txthumedad.setText(humedad + " %");
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        groweasydb.child("Luz").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String luz = dataSnapshot.child("luz").getValue().toString();
+                    txtluz.setText(luz);
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        groweasydb.child("T_ambiente").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String t_ambiente = dataSnapshot.child("t_ambiente").getValue().toString();
+                    txtambiente.setText(t_ambiente + " °C");
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        groweasydb.child("T_suelo").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String t_suelo = dataSnapshot.child("t_suelo").getValue().toString();
+                    txtsuelo.setText(t_suelo + " °C");
                 }
 
             }
