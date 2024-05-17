316314a27f3d7097bebf851c944a9fe215a728cf
gabriel <gabriel2000@outlook.cl>
2024-05-17 12:32:36 -0400

    importacion de firebase al proyecto

--- app/src/main/java/com/example/groweasy/MainActivity.java
+++ app/src/main/java/com/example/groweasy/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private Button btnIngresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
