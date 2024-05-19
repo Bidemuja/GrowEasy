11eb151a2f5ccf0d93c4de760a73f0520056f80d
Bianca <bianca.munozjaramillo@gmail.com>
2024-05-19 15:21:42 -0400

    Se agrega menu lateral a la clase VistaPrincipal.java

--- app/src/main/java/com/example/groweasy/VistaPrincipal.java
+++ app/src/main/java/com/example/groweasy/VistaPrincipal.java
@@ -2,35 +2,59 @@ package com.example.groweasy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class VistaPrincipal extends AppCompatActivity {
+public class VistaPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DatabaseReference groweasydb;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     Button btnImgMenu, btnHumedad;
     TextView txthumedad, txtsuelo, txtluz, txtambiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_principal);
-        /*btnImgMenu = (Button) findViewById(R.id.imgLogoMenu);
-        btnHumedad = (Button) findViewById(R.id.btnHumedad);
 
-        txthumedad= (TextView) findViewById(R.id.txthumedad);
-        txtluz= (TextView) findViewById(R.id.txtluz);
-        txtambiente= (TextView) findViewById(R.id.txtambiente);
-        txtsuelo= (TextView) findViewById(R.id.txtsuelo);
+        /*--------------------Hooks-------------------------*/
+        drawerLayout = findViewById(R.id.dlPrincipal);
+        navigationView = findViewById(R.id.nvMenuLateral);
+        toolbar = findViewById(R.id.tbPrincipal);
+
+        /*-------------------Tool Bar ---------------------*/
+        setSupportActionBar(toolbar);
+
+        /*------------------Navigation Drawer Menu --------*/
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        btnHumedad = (Button) findViewById(R.id.btnDashboardHumedad);
+
+        txthumedad= (TextView) findViewById(R.id.txtVariableHumedad);
+        txtluz= (TextView) findViewById(R.id.txtVariableLuz);
+        txtambiente= (TextView) findViewById(R.id.txtVariableAmbiente);
+        txtsuelo= (TextView) findViewById(R.id.txtVariableSuelo);
         groweasydb= FirebaseDatabase.getInstance().getReference();
 
         groweasydb.child("Humedad").addValueEventListener(new ValueEventListener() {
@@ -93,14 +117,6 @@ public class VistaPrincipal extends AppCompatActivity {
             }
         });
 
-        btnImgMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(VistaPrincipal.this, Inicio.class);
-                startActivity(intent);
-            }
-        });
 
         btnHumedad.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -109,7 +125,12 @@ public class VistaPrincipal extends AppCompatActivity {
                 intent = new Intent(VistaPrincipal.this, Dashboard.class);
                 startActivity(intent);
             }
-        });*/
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 
 }
